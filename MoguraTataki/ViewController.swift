84769167deaf71fefed3bb4978ef8c31e2b7383b
shuwarz <imatakakun@gmail.com>
2020-06-05 19:37:49 +0900

    Initial Commit

---
+++ MoguraTataki/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoguraTataki
+//
+//  Created by 今泉　孝陽 on 2020/06/05.
+//  Copyright © 2020 app.com.Takaharu.Imaizumi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

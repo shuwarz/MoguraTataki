f7906e51f741e0755c8438ce81c033a91868dde0
shuwarz <imatakakun@gmail.com>
2020-06-19 20:11:16 +0900

    タイマーのコードを７行書いた トップ、ゲーム、リザルトの三つのページを作った。

---
+++ MoguraTataki/ResultViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ResultViewController.swift
+//  MoguraTataki
+//
+//  Created by 今泉　孝陽 on 2020/06/19.
+//  Copyright © 2020 app.com.Takaharu.Imaizumi. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    @IBOutlet var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MoguraTataki/TopViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  MoguraTataki
+//
+//  Created by 今泉　孝陽 on 2020/06/05.
+//  Copyright © 2020 app.com.Takaharu.Imaizumi. All rights reserved.
+//
+
+import UIKit
+
+class TopViewController: UIViewController {
+
+    @IBOutlet var label: UILabel!
+    
+    override func viewDidLoad() {
+           super.viewDidLoad()
+            // Do any additional setup after loading the view.
+        
+    }
+
+    @IBAction func start() {
+        
+    }
+    
+    @IBAction func stop() {
+        
+    }
+    
+    
+}
+
+        

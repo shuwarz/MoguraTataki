ae471d25bf54652bf77b27ad50e833020e067c0f
shuwarz <imatakakun@gmail.com>
2020-06-26 20:12:14 +0900

    イメージビューを押すとポイントが増えるようになる機能と、結果画面で『結果発表！！！！！』と出るようになる この二つができた。 次回までの宿題は、ポイント計算できるようにする！！

--- MoguraTataki/GameViewController.swift
+++ MoguraTataki/GameViewController.swift
@@ -9,16 +9,84 @@
 import UIKit
 
 class GameViewController: UIViewController {
-
+    var number: Int = 0
+    
     @IBOutlet var label: UILabel!
+    var count: Float = 0
+    
+    var timer: Timer = Timer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        count = 60
+        
+        if !timer.isValid {
+            //タイマーが動作していなかったら動かす
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.up), userInfo: nil, repeats: true)
+        }
+        
     }
     
+    @objc func up() {
+        //countを1引く
+        count = count - 1
+        print("ビスコのじかん")
+        print(count)
     
+        label.text = String(count)
+        
+    }
+    
+    @IBAction func tapActionUe1Hidari(_ sender: Any) {
+        number += 2
+    }
+
+    @IBAction func tapActionUe1Naka(_ sender: Any) {
+        number += 1
+    }
+
+    @IBAction func tapActionUe1Migi(_ sender: Any) {
+        number += 1
+    }
+
+    @IBAction func tapActionUe2Hidari(_ sender: Any) {
+        number -= 3
+    }
+
+    @IBAction func tapActionUe2Naka(_ sender: Any) {
+        number -= 3
+    }
+
+    @IBAction func tapActionUe2Migi(_ sender: Any) {
+        number += 1
+    }
+
+    @IBAction func tapActionUe3Hidari(_ sender: Any) {
+        number += 2
+    }
+
+    @IBAction func tapActionUe3Naka(_ sender: Any) {
+        number -= 3
+    }
+
+    @IBAction func tapActionUe3Migi(_ sender: Any) {
+        number += 2
+    }
+
+    @IBAction func tapActionUe4Hidari(_ sender: Any) {
+        number += 1
+    }
+
+    @IBAction func tapActionUe4Naka(_ sender: Any) {
+        number -= 3
+    }
+
+    @IBAction func tapActionUe4migi(_ sender: Any) {
+        number += 2
+    }
+
 
     /*
     // MARK: - Navigation
